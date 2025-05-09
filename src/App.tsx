54a5ad39d9415b568a5e7f6a156f30ea1900f1c5
inevadim <kvadrocheyk@gmail.com>
2025-05-09 18:47:22 +0300

    deploy

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styles from './App.module.scss'
+
+const App = () => {
+
+  return (
+    <div className={styles.app}>
+      BASIC PROJECT INEVADIM
+    </div>
+  )
+}
+
+export default App
